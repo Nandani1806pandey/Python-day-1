ee16f82e60821b9be50ae539e3189035bd72f985
NandaniPandey <Nandani1806pandey@gmail.comgit push -u origin maingit config --global user.emial Nandani1806pandey@gmail.com>
2025-09-04 15:19:50 +0530

    Second commit

--- 01_python/hello.py
+++ 01_python/hello.py
@@ -2,8 +2,8 @@ print("Welcome to the tip calculator! ")
 bill = float(input("What was the total bill? $"))
 tip = int(input("How much tip would you like to give? 10, 12, 0r 15? "))
 people = int(input("How many people to split the bill? "))
-result = tip/100 
-tip_amount = bill * result
+tip_percentage = tip/100 
+tip_amount = bill * tip_percentage
 total = tip_amount + bill
 peoples = total / people
 final_amount = round(peoples, 2)
